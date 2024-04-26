2aef438dd27cb396554f95c40ca3296ddeface2e
Aidin Almasbekov <donkasta07@gmail.com>
2024-04-26 07:33:13 +0600

    start project

---
+++ app/src/main/java/com/learnproject/MainActivity.java
@@ -0,0 +1,14 @@
+package com.learnproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
